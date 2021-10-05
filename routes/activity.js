ca50d066b552be3eb6dfd899c6e1028f22dcd015
impetiue <83606663+impetiue@users.noreply.github.com>
2021-10-05 19:19:10 +0530

    Add files via upload

--- routes/activity.js
+++ routes/activity.js
@@ -80,7 +80,7 @@ exports.save = function (req, res) {
     // Data from the req and put it in an array accessible to the main app.
     console.log( req.body );
     logData(req);
-    res.send(200, 'Save');
+    res.send(200, 'ok');
 };
 
 /*
