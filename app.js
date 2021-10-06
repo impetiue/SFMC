b07ac85faa125607b70ced631e3f783e78104e81
impetiue <83606663+impetiue@users.noreply.github.com>
2021-10-06 17:38:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Express in Development Mode
 if ('development' == app.get('env')) {
-  app.use(express.errorhandler());
+  app.use(errorhandler());
 }
 
 // HubExchange Routes
@@ -33,11 +33,18 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save/', activity.save );
-app.post('/journeybuilder/validate/', activity.validate );
-app.post('/journeybuilder/publish/', activity.publish );
-app.post('/journeybuilder/execute/', activity.execute );
+// app.post('/journeybuilder/save/', activity.save );
+// app.post('/journeybuilder/validate/', activity.validate );
+// app.post('/journeybuilder/publish/', activity.publish );
+// app.post('/journeybuilder/execute/', activity.execute );
+app.post(/\/activity\/(save|publish|validate)/, (req, res) => {
+	verifyJwt(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret, (err, decoded) => {
+		// verification error -> unauthorized request
+		if (err)	return res.status(401).end();
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+		return res.status(200).json({success: true});
+	});
+
+// http.createServer(app).listen(app.get('port'), function(){
+//   console.log('Express server listening on port ' + app.get('port'));
 });
