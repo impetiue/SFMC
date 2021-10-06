dada041316df716bf130b503c71617a6e3a55187
impetiue <83606663+impetiue@users.noreply.github.com>
2021-10-06 15:40:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var http        = require('http');
 var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
-var activity    = require('./routes/journeybuilder');
+var activity    = require('./routes/activity');
 
 var app = express();
 
@@ -33,10 +33,10 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save', journeybuilder.save );
-app.post('/journeybuilder/validate', journeybuilder.validate );
-app.post('/journeybuilder/publish', journeybuilder.publish );
-app.post('/journeybuilder/execute', journeybuilder.execute );
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
